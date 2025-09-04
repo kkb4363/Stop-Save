25c166b873de49b10b4b70dd6e675e55957273e5
Gibeom <101088024+kkb4363@users.noreply.github.com>
2025-09-04 17:59:10 +0900

    lst

--- backend/save/src/main/java/stop/save/config/SecurityConfig.java
+++ backend/save/src/main/java/stop/save/config/SecurityConfig.java
@@ -55,7 +55,8 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(Arrays.asList("https://stop-save.vercel.app"));
+        configuration.setAllowedOriginPatterns(Arrays.asList("https://stop-save.vercel.app",
+         "https://651f81414594.ngrok-free.app/"       ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
